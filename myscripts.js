d7eee5466f38be8af4912d61f0f48d059effd59c
Jeb Carter <frederickjcarter@gmail.com>
2020-04-20 12:21:37 -0400

    fade in divs

---
+++ myscripts.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function()) {
+
+  /* When window is scrolled */
+  $(window).scroll(function() {
+
+    let divs = document.querySelectorAll('.main1,.main2');
+
+    /* Find location of each element */
+    $(divs).each(function(i) {
+
+      var top_of_div = $(this).position().top;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the div is visible, fade it in */
+      if (bottom_of_window > top_of_div) {
+        $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  });
+
+});
