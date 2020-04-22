f4ef89132bed1bcfd92aaa6530d8add7179af79e
Jeb Carter <frederickjcarter@gmail.com>
2020-04-21 20:20:37 -0400

    tweak slide in

--- myscripts.js
+++ myscripts.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
     /* Find location of each element */
     $(divs).each(function(i) {
 
-      var top_of_div = $(this).offset().top + 60;
+      var top_of_div = $(this).offset().top + 40;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       /* If the div is visible, fade it in */
       if (bottom_of_window > top_of_div) {
-        $(this).animate({'opacity':'1'},1000);
+        $(this).animate({'opacity':'1'},1300);
         $(this).addClass("slide-in");
       }
 
